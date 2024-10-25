bf840f5b5abbc3bb634c6c4e4a6817244a7008ab
zamit00 <zamit00@gmail.com>
2024-10-25 20:10:17 +0300

    Update simulatorjvs.js

--- simulatorjvs.js
+++ simulatorjvs.js
@@ -1,10 +1,13 @@
 function highlightCell(cell) {
     // מחזירים את כל התאים למצבם המקורי
     const cells = document.querySelectorAll('#choosesikon td');
-    cells.forEach(c => {
+    cells.forEach(cell=> {
         c.style.backgroundColor = 'black'; // צבע רקע שחור
     });
 
     // משנים את צבע הרקע של התא שנלחץ
     cell.style.backgroundColor = 'green'; // צבע רקע ירוק
+    alert(cell.getAttribute("data-value"));
+    
+    
 }
