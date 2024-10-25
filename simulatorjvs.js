f886663b1eff294e08d2f42c0e0ea7e871312bf4
zamit00 <zamit00@gmail.com>
2024-10-26 01:31:33 +0300

    Update simulatorjvs.js

--- simulatorjvs.js
+++ simulatorjvs.js
@@ -121,7 +121,7 @@ if (isNaN(dn) || dn < 0 || dn > 1) {
     else if (document.getElementById("rd2").checked){x1=x1;x2=0;}
     else{x1=0;x2=x2;}
 
-    sumsum(x1, x2, dn, ribitchoose, ribitOzar,tkofabeshanim)
+    sumsum(x1, x2, dn, ribitchoose, ribitOzar,columnIndex);
 
 
 }
