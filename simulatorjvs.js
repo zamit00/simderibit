282d50c5df5b8817ce9a1e6e63f4057ebbc7b0b4
zamit00 <zamit00@gmail.com>
2024-10-26 05:27:05 +0300

    Update simulatorjvs.js

--- simulatorjvs.js
+++ simulatorjvs.js
@@ -63,7 +63,7 @@ sumsum(x1, x2, dn, ribitchoose, ribitOzar,tkofabeshanim,columnIndex)
 function beforsumsum(){
     const cells = document.querySelectorAll('#choosesikon td');
 let greenCellFound = false; // Flag to track if any green cell is found
-
+alert('1');
 cells.forEach(cell => {
     if (cell.style.backgroundColor === 'green') {
         ribitchoose = cell.getAttribute("data-value");
@@ -113,7 +113,7 @@ if (!greenCellFound) {
 }
 else{
     
-
+alert('2');
 
     //if(isNaN(ribitchoose)){alert("");return;}//
     let x1 = parseInt(document.getElementsByName("txt1")[0].value);
@@ -133,7 +133,7 @@ if (isNaN(dn) || dn < 0 || dn > 1) {
 }
 
     
-    
+    alert('3');
    
     let tkofabeshanim = parseInt(document.getElementById("selecttkofa").value);
     let ribitOzar = 0.04;
